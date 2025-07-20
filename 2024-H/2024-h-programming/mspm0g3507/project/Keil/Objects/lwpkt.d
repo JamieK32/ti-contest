6889fbc28bd94c159cc121128a4b0dfcf3fe9ffc
JamieK <153492048+JamieK32@users.noreply.github.com>
2025-07-20 21:19:13 +0800

    工程规范化

--- 2024-H/2024-h-programming/mspm0g3507/project/Keil/Objects/lwpkt.d
+++
@@ -1,6 +0,0 @@
-./objects/lwpkt.o: \
-  ..\..\custom_src\middleware\communication\lwpkt\lwpkt.c \
-  ..\..\custom_src\middleware\communication\lwpkt\lwpkt.h \
-  ..\..\custom_src\middleware\communication\lwpkt\lwpkt_opt.h \
-  ..\..\custom_src\middleware\communication\lwpkt\lwpkt_opts.h \
-  ..\..\custom_src\middleware\communication\lwrb\lwrb.h
--- 2024-H/2024-h-programming/mspm0g3507/project/Keil/Objects/lwrb.d
+++
@@ -1,3 +0,0 @@
-./objects/lwrb.o: ..\..\custom_src\middleware\communication\lwrb\lwrb.c \
-  ..\..\custom_src\middleware\communication\lwrb\lwrb_opts.h \
-  ..\..\custom_src\middleware\communication\lwrb\lwrb.h
